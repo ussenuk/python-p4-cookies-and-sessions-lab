0e01947e30953b65733c93990f8e1cbe13f1d5c9
ussenuk <ukimanuka@gmail.com>
2024-03-17 12:50:48 +0300

    working on the sookies and session

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session, request, render_template_string
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -17,18 +17,81 @@ db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
-    session['page_views'] = 0
+    # session['page_views'] = 0
+    session.pop('page_views', default=None)
     return {'message': '200: Successfully cleared session data.'}, 200
 
 @app.route('/articles')
 def index_articles():
 
-    pass
 
-@app.route('/articles/<int:id>')
+    articles = [article.to_dict() for article in Article.query.all()]
+    
+    response = make_response(
+        jsonify(articles),
+        200
+    )
+
+    return response
+
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
 
-    pass
+    
+
+    
+    if request.method == 'GET':
+
+        # count = int(request.cookies.get('visited_article count', 0))
+        # session['page_views'] = 0
+        
+        
+        # count = count+1
+        if 'page_views' in session:
+            session['page_views'] = session.get('page_views') + 1
+
+        else: 
+            session['page_views'] = 1
+
+        if 'page_views' in session and session['page_views'] > 3:
+            return make_response(
+            jsonify({'message': 'Maximum pageview limit reached'}),
+            401
+        )
+
+
+
+
+    
+        article = Article.query.filter_by(id=id).first()
+
+
+
+        article_dict = article.to_dict()
+
+        response = make_response(
+            jsonify(article_dict),
+            200
+        )
+
+
+        # response.set_cookie('visited_article count', str(count))
+
+
+
+        return response
+    
+# @app.route('/get_session')
+# def get_session():
+    
+#         return render_template_string("""
+#             {% if session['page_views'] %}
+#                 <h1> {{ session['page_views'] }}!</h1>
+#             {% else %}
+#                 <h1>Nothing</h1>
+#             {% endif %}
+#         """)
+     
 
 if __name__ == '__main__':
     app.run(port=5555)
